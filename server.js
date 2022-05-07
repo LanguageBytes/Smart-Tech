4ba66cab4b93dd0785e7f4049f35fd4a50905323
Sarah Lloyd <sarahlloyd407a@gmail.com>
2022-05-07 13:23:27 +0100

    Creating server file, installing dependencies

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const handlebars = require("express-handlebars");
+
+const router = require("./controllers");
+const sequelize = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const hbs = handlebars.create();
+
+app.use(
+  session({
+    secret: "",
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+  })
+);
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(router);
+
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port: ${PORT}`);
+  });
+});
+
+console.log();
