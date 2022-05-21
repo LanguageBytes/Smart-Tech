1fa61de21a893279f0c08be71483446fb8322cbb
hassanmahdi58 <hassanmahdi58@gmail.com>
2022-05-21 15:24:34 +0100

    login user

--- public/js/login.js
+++ public/js/login.js
@@ -1,23 +1,54 @@
-const loginFormHandler = async function(event) {
-    event.preventDefault();
-  
-    const enterUsername= document.querySelector('#username-login');
-    const enterPassword = document.querySelector('#password-login');
-  
-    const response = await fetch('/api/user/login', {
-      method: 'POST',
-      body: JSON.stringify({
-        username: enterUsername.value,
-        password: enterPassword.value,
-      }),
-      headers: { 'Content-Type': 'application/json' },
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (email && password) {
+
+    const response = await fetch("/api/user/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
     });
-  
+
     if (response.ok) {
-      document.location.replace('/dashboard');
+     
+      document.location.replace("/");
     } else {
-      alert('Log in failed. Please try again.');
+      document.getElementById("login-email").innerHTML =
+        "Credentials dont exist, signup instead";
+     
     }
-  };
-  
-  document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
+  }
+};
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector("#name-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (name && email && password) {
+    const response = await fetch("/api/user", {
+      method: "POST",
+      body: JSON.stringify({ name, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
