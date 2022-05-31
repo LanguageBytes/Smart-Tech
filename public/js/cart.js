e27b6f30a9a30cd99817114c1c3a5aa72e35180f
Sarah Lloyd <sarahlloyd407a@gmail.com>
2022-05-31 09:07:59 +0100

    Products, index

---
+++ public/js/cart.js
@@ -0,0 +1,21 @@
+const response = await fetch(`/api/products/newAdd`, {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      description,
+      price,
+     
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert("Failed to create new product");
+  }
+;
+
+document.querySelector(".addProducts").addEventListener("submit", newFormHandler);
